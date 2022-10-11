56071415c7135ccb4567e2f5d4e56bf06b23d8f8
amtojkaur <amtojk@uw.edu>
2022-10-11 16:58:36 -0700

    Added Java Documentation.

--- src/Minesweeper.java
+++ src/Minesweeper.java
@@ -1,6 +1,26 @@
+/**
+ * TCSS 360
+ * Instructor: Tom Capaul
+ * Personal Assignment 1 - MineSweeper.java
+ */
+
 import java.io.*;
 import java.util.*;
+
+/**
+ * Displays minesweeper minefield output with all adjacency values and bombs.
+ *
+ * @author Amtoj Kaur - amtojkaur1321@gmail.com
+ */
 public class Minesweeper {
+
+    /**
+     * Instantiates Scanner for the program as well as calls all methods needed to run code
+     * it also throws FileNotFoundException.
+     *
+     * @param theArgs
+     * @throws FileNotFoundException
+     */
     public static void main(final String[] theArgs) throws FileNotFoundException {
         Scanner scanner = new Scanner(System.in);
         int numFields = 0;
@@ -17,6 +37,15 @@ public class Minesweeper {
         scanner.close();
     }
 
+    /**
+     * This method iterates through the given file and reads input to character array.
+     * It also calls displayOut method and fillField method.
+     *
+     * @param theRow
+     * @param theCol
+     * @param theNumFields
+     * @param theScanner
+     */
     private static void mineSweeper(final int theRow, final int theCol, final int theNumFields, final Scanner theScanner) {
         int numFields = theNumFields;
         int row = theRow;
@@ -40,6 +69,12 @@ public class Minesweeper {
         }
     }
 
+    /**
+     * This method formats the display of the minefield as well as increments the field #.
+     *
+     * @param theFilledMine
+     * @param theNumFields
+     */
     private static void displayOut(final char[][] theFilledMine, int theNumFields) {
         System.out.println("Field #" + theNumFields + ":");
         for (char[] chars : theFilledMine) {
@@ -49,6 +84,15 @@ public class Minesweeper {
             System.out.println();
         }
     }
+
+    /**
+     * This method goes through the array created by reading from system.in, and iterates through
+     * and returns a character array that has bombs where bombs should be and adjacency
+     * values in 'safe' spaces. Calls adjacent method to calculate adjacency value.
+     *
+     * @param theMineField
+     * @return numFields
+     */
     private static char[][] fillField(final char[][] theMineField) {
         char[][] numFields = new
                 char[theMineField.length][theMineField[0].length];
@@ -64,6 +108,15 @@ public class Minesweeper {
         return numFields;
     }
 
+    /**
+     * This method calculates adjacency and calls validValue to help it it then returns
+     * the integer value cast as a char to fillField method.
+     *
+     * @param theField
+     * @param theRow
+     * @param theCol
+     * @return myCount
+     */
     private static char adjacent(final char[][] theField, final int theRow, final int theCol) {
         int myCount = 0;
         if(validValue(theField, theRow - 1, theCol - 1)) {
@@ -81,7 +134,6 @@ public class Minesweeper {
                 myCount++;
             }
         }
-        //current row:
         if(validValue(theField, theRow, theCol - 1)) {
             if(theField[theRow][theCol - 1] == '*') {
                 myCount++;
@@ -92,7 +144,6 @@ public class Minesweeper {
                 myCount++;
             }
         }
-        //row below:
         if(validValue(theField, theRow + 1, theCol - 1)) {
             if(theField[theRow + 1][theCol - 1] == '*') {
                 myCount++;
@@ -108,11 +159,18 @@ public class Minesweeper {
                 myCount++;
             }
         }
-        //System.out.println(myCount);
         return (char)(myCount + '0');
 
    }
 
+    /**
+     * This checks for validity of position  for row and col passed and tells it to adjacent method.
+     *
+     * @param theMineField
+     * @param theRow
+     * @param theCol
+     * @return boolean
+     */
    private static boolean validValue( final char[][] theMineField, final int theRow, final int theCol) {
        return theRow < theMineField.length && theRow >= 0 && theCol < theMineField[0].length && theCol >= 0;
 
