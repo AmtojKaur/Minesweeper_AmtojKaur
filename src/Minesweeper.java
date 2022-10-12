7791b0dbe58d2425dd8da272df06e810ad6ecaa2
amtojkaur <amtojk@uw.edu>
2022-10-11 21:55:29 -0700

    Added Java Documentation.

--- src/Minesweeper.java
+++ src/Minesweeper.java
@@ -4,13 +4,13 @@
  * Personal Assignment 1 - MineSweeper.java
  */
 
-import java.io.*;
-import java.util.*;
+import java.io.PrintStream;
+import java.util.Scanner;
 
 /**
  * Displays minesweeper minefield output with all adjacency values and bombs.
  *
- * @author Amtoj Kaur - amtojkaur1321@gmail.com
+ * @author Amtoj Kaur
  */
 public class Minesweeper {
 
@@ -19,22 +19,13 @@ public class Minesweeper {
      * it also throws FileNotFoundException.
      *
      * @param theArgs
-     * @throws FileNotFoundException
      */
-    public static void main(final String[] theArgs) throws FileNotFoundException {
-        Scanner scanner = new Scanner(System.in);
-        int numFields = 0;
-        if (theArgs.length == 1) {
-            try {
-                scanner = new Scanner(new File(theArgs[0]));
-                int row = scanner.nextInt();
-                int col = scanner.nextInt();
-                mineSweeper(row, col, numFields, scanner);
-            } catch (FileNotFoundException e) {
-                throw new FileNotFoundException("File not found" + e);
-            }
-        }
-        scanner.close();
+    public static void main(final String[] theArgs) {
+        final Scanner scanner = new Scanner(System.in);
+        final int numFields = 0;
+        final int row = scanner.nextInt();
+        final int col = scanner.nextInt();
+        mineSweeper(row, col, numFields, scanner);
     }
 
     /**
@@ -46,22 +37,23 @@ public class Minesweeper {
      * @param theNumFields
      * @param theScanner
      */
-    private static void mineSweeper(final int theRow, final int theCol, final int theNumFields, final Scanner theScanner) {
+    private static void mineSweeper(final int theRow, final int theCol, final int theNumFields,
+                                    final Scanner theScanner) {
         int numFields = theNumFields;
         int row = theRow;
         int col = theCol;
         while (!(row == 0 || col == 0)) {
             numFields++;
-            char[][] field = new char[row + 1][col + 1];
+            final char[][] field = new char[row + 1][col + 1];
             for (int i = 0; i < row + 1; i++) {
-                char[] line = theScanner.nextLine().toCharArray();
+                final char[] line = theScanner.nextLine().toCharArray();
                 for (int j = 0; j < col; j++) {
                     if (j > 0 && i > 0) {
                         field[i][j] = line[j];
                     }
                 }
             }
-            char[][] filledMineField = fillField(field);
+            final char[][] filledMineField = fillField(field);
             displayOut(filledMineField, numFields);
             row = theScanner.nextInt();
             col = theScanner.nextInt();
@@ -75,16 +67,19 @@ public class Minesweeper {
      * @param theFilledMine
      * @param theNumFields
      */
-    private static void displayOut(final char[][] theFilledMine, int theNumFields) {
-        System.out.println("Field #" + theNumFields + ":");
+    private static void displayOut(final char[][] theFilledMine,
+                                   final int theNumFields) {
+        final PrintStream console = new PrintStream(System.out);
+        System.setOut(console);
+
+        console.println("Field #" + theNumFields + ":");
         for (char[] chars : theFilledMine) {
             for (int j = 0; j < theFilledMine[0].length; j++) {
-                System.out.print(chars[j]);
+                console.print(chars[j]);
             }
-            System.out.println();
+            console.println();
         }
     }
-
     /**
      * This method goes through the array created by reading from system.in, and iterates through
      * and returns a character array that has bombs where bombs should be and adjacency
@@ -94,22 +89,26 @@ public class Minesweeper {
      * @return numFields
      */
     private static char[][] fillField(final char[][] theMineField) {
-        char[][] numFields = new
+        final char[][] numFields = new
                 char[theMineField.length][theMineField[0].length];
-        for(int i = 0; i < theMineField.length; i++) {
-            for(int j = 0; j < theMineField[0].length; j++) {
-                if(theMineField[i][j] == '*') {     //bomb
+        int i = 0;
+        while (i < theMineField.length) {
+            int j = 0;
+            while (j < theMineField[0].length) {
+                if (theMineField[i][j] == '*') {
                     numFields[i][j] = '*';
-                } else { //safe space
+                } else {
                     numFields[i][j] = adjacent(theMineField, i, j);
                 }
+                j++;
             }
+            i++;
         }
         return numFields;
     }
 
     /**
-     * This method calculates adjacency and calls validValue to help it it then returns
+     * This method calculates adjacency and calls validValue to help it then returns
      * the integer value cast as a char to fillField method.
      *
      * @param theField
@@ -118,51 +117,45 @@ public class Minesweeper {
      * @return myCount
      */
     private static char adjacent(final char[][] theField, final int theRow, final int theCol) {
-        int myCount = 0;
-        if(validValue(theField, theRow - 1, theCol - 1)) {
-                if(theField[theRow - 1][theCol - 1] == '*'){
-                myCount++;
-            }
+        int count = 0;
+        if (validValue(theField, theRow - 1, theCol - 1)
+                && theField[theRow - 1][theCol - 1] == '*') {
+            count++;
         }
-        if(validValue(theField, theRow - 1, theCol)) {
-            if(theField[theRow - 1][theCol] == '*') {
-                myCount++;
-            }
+        if (validValue(theField, theRow - 1, theCol)
+                && theField[theRow - 1][theCol] == '*') {
+            count++;
         }
-        if(validValue(theField, theRow - 1, theCol + 1)) {
-            if(theField[theRow - 1][theCol + 1] == '*') {
-                myCount++;
-            }
+        if (validValue(theField, theRow - 1, theCol + 1)
+                && theField[theRow - 1][theCol + 1] == '*') {
+            count++;
         }
-        if(validValue(theField, theRow, theCol - 1)) {
-            if(theField[theRow][theCol - 1] == '*') {
-                myCount++;
-            }
+        //current row:
+        if (validValue(theField, theRow, theCol - 1)
+                && theField[theRow][theCol - 1] == '*') {
+            count++;
         }
-        if(validValue(theField, theRow, theCol + 1)) {
-            if(theField[theRow][theCol + 1] == '*') {
-                myCount++;
-            }
+        if (validValue(theField, theRow, theCol + 1)
+                && theField[theRow][theCol + 1] == '*') {
+            count++;
         }
-        if(validValue(theField, theRow + 1, theCol - 1)) {
-            if(theField[theRow + 1][theCol - 1] == '*') {
-                myCount++;
-            }
+        //row below:
+        if (validValue(theField, theRow + 1, theCol - 1)
+                && theField[theRow + 1][theCol - 1] == '*') {
+            count++;
         }
-        if(validValue(theField, theRow + 1, theCol)) {
-            if(theField[theRow + 1][theCol] == '*') {
-                myCount++;
-            }
+        if (validValue(theField, theRow + 1, theCol)
+                && theField[theRow + 1][theCol] == '*') {
+            count++;
         }
-        if(validValue(theField, theRow + 1, theCol + 1)) {
-            if(theField[theRow + 1][theCol + 1] == '*') {
-                myCount++;
-            }
+        if (validValue(theField, theRow + 1, theCol + 1)
+                && theField[theRow + 1][theCol + 1] == '*') {
+            count++;
         }
-        return (char)(myCount + '0');
+        //System.out.println(myCount);
+        return (char) (count + '0');
 
-   }
-
+    }
     /**
      * This checks for validity of position  for row and col passed and tells it to adjacent method.
      *
@@ -171,8 +164,10 @@ public class Minesweeper {
      * @param theCol
      * @return boolean
      */
-   private static boolean validValue( final char[][] theMineField, final int theRow, final int theCol) {
-       return theRow < theMineField.length && theRow >= 0 && theCol < theMineField[0].length && theCol >= 0;
+    private static boolean validValue(final char[][] theMineField,
+                                      final int theRow, final int theCol) {
+        return theRow < theMineField.length && theRow >= 0
+                && theCol < theMineField[0].length && theCol >= 0;
 
-   }
+    }
 }
